cffb339ea37c690242e1350ad266d862376a4774
Andy Wahrenberger <andywahr@microsoft.com>
2020-03-24 01:00:51 -0500

    updates to show loggin and blob interactions

---
+++ sample-cdn-api-dotnetcore/sample-cdn-api-dotnetcore/Controllers/ValueController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Auth;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace sample_cdn_api_dotnetcore.Controllers
+{
+    [Route("api/Value")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        private ILogger<ValueController> _logger;
+        private IConfiguration _config;
+
+        public ValueController(ILogger<ValueController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpPost("SaveRequest")]
+        public async Task<IActionResult> SaveRequest(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("SaveRequest");
+
+            string request = string.Empty;
+            using (var reader = new StreamReader(Request.Body))
+            {
+                request = await reader.ReadToEndAsync();
+            }
+
+            var azureTokenProvider = new AzureServiceTokenProvider();
+            string accessToken = await azureTokenProvider.GetAccessTokenAsync($"https://{_config["StorageAccountName"]}.blob.core.windows.net", cancellationToken: cancellationToken);
+            TokenCredential tokenCredential = new TokenCredential(accessToken);
+
+            StorageCredentials storageCredentials = new StorageCredentials(tokenCredential);
+
+            CloudBlobClient blobClient = new CloudBlobClient(new StorageUri(new Uri($"https://{_config["StorageAccountName"]}.blob.core.windows.net")), storageCredentials);
+            CloudBlobContainer container = blobClient.GetContainerReference("loans");
+            CloudBlockBlob blob = container.GetBlockBlobReference($"{Guid.NewGuid().ToString("n")}.json");
+
+            await blob.UploadTextAsync(request, cancellationToken);
+
+            return Ok();
+        }
+
+        [HttpGet("ResilentExternalCall")]
+        public async Task<string> ResilentExternalCall()
+        {
+            ValueServices service = new ValueServices();
+            using (CancellationTokenSource cts = new CancellationTokenSource())
+            {
+                var serviceTask = service.GetValue(cts.Token);
+
+                if (!Task.WaitAll(new Task[] { serviceTask }, 2000))
+                {
+                    cts.Cancel();
+                    _logger.LogWarning("ResilentExternalCall:  Overlimit");
+                    return await Task.FromResult("-1");
+                }
+                else
+                {
+                    _logger.LogDebug("ResilentExternalCall:  From Live");
+                    return await Task.FromResult(serviceTask.Result.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ sample-cdn-api-dotnetcore/sample-cdn-api-dotnetcore/ValueServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sample_cdn_api_dotnetcore
+{
+    public class ValueServices
+    {
+        static Random random = new Random();
+
+        public async Task<int> GetValue(System.Threading.CancellationToken token)
+        {
+            int waitNumberOfSeconds = random.Next(4);
+
+            for (int ii = 0; ii < waitNumberOfSeconds; ii++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    return -1;
+                }
+                await Task.Delay(1000);
+            }
+
+            return waitNumberOfSeconds;
+        }
+    }
+}
